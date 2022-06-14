fa158caaf914783ccae705c840dadfa1bdfdc55f
Doseq <kontakt@doseq.pl>
2022-06-14 18:46:01 +0200

    Added register user DTO

---
+++ src/main/java/pl/doseq/chatter/user/model/RegisterUserDto.java
@@ -0,0 +1,25 @@
+package pl.doseq.chatter.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+public class RegisterUserDto {
+
+    @NotNull
+    @NotBlank
+    private String username;
+    @NotNull
+    @NotBlank
+    private String password;
+
+}
+
